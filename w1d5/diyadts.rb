cdc221098cf7902b0a4f6577ff8d783ee0a818a0
Lindsey <lewhitley@gmail.com>
2016-09-15 22:04:41 -0700

    add MyMap class

--- w1d5/diyadts.rb
+++ w1d5/diyadts.rb
@@ -55,3 +55,76 @@ end
 # queue.show
 # queue.enqueue(3)
 # queue.show
+
+class MyMap
+  def initialize
+    @map = []
+  end
+
+  def assign(key,value)
+    if !key_exists?(key)
+      @map << [key,value]
+    else
+      reassign(key,value)
+    end
+  end
+
+  def lookup(key)
+    if key_exists?(key)
+      p value(key)
+    else
+      puts "That key does not exist"
+    end
+  end
+
+  def remove(key)
+    if key_exists?(key)
+      @map.delete_at(key_index(key))
+    else
+      p "That key does not exist"
+    end
+    nil
+  end
+
+  def show
+    p @map
+  end
+
+  private
+
+  def reassign(key,value)
+    @map[key_index(key)] = [key,value]
+  end
+
+  def keys
+    keys = []
+    return [] if @map == []
+    @map.each {|pair| keys << pair[0]}
+    keys
+  end
+
+  def value(key)
+    @map[key_index(key)][1]
+  end
+
+  def key_exists?(key)
+    return true if keys.include?(key)
+    false
+  end
+
+  def key_index(key)
+    keys.each_with_index do |check_key, i|
+      return i if check_key == key
+    end
+  end
+end
+
+map = MyMap.new
+map.assign(2,true)
+map.show
+map.assign(2,false)
+map.assign(3,true)
+map.show
+map.remove(2)
+map.lookup(2)
+map.lookup(3)
